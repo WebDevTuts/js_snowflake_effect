6e9f4e6a4cfe920f5ac3f781b4f9e8d2b05ad5df
edwinchen85 <edwinchen85@gmail.com>
2017-05-13 21:34:30 +0800

    Loop through the empty flakes and apply attributes

--- snow.js
+++ snow.js
@@ -13,4 +13,14 @@ window.onload = function() {
   // generate the snowflakes and apply attributes
   var mf = 100;   // max flakes
   var flakes = [];
+
+  // loop through the empy flakes and apply attributes
+  for ( var i = 0; i < mf; i++ ) {
+    flakes.push({
+      x: Math.random() * W,
+      y: Math.random() * H,
+      r: Math.random() * 5 + 2, // min of 2px and max of 7px
+      d: Math.random() + 1      // density of the flake
+    })
+  }
 }
